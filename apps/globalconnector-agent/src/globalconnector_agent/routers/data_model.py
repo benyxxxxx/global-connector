aff71f1e93457717da0a373efdf47878502a6a1b
Classic Neupane <classicneupane@gmail.com>
2025-07-29 17:52:58 +0545

    feat(agent): add service agent

--- apps/globalconnector-agent/src/globalconnector_agent/routers/data_model.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-from config import Config
-
-
-class ServiceBotRequest(BaseModel):
-    user_message: str = ""
-    user_id: str = Config.USER_ID
-    request_id: str = Config.SESSION_ID
--- apps/globalconnector-agent/src/globalconnector_agent/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .utils import get_auth_token
-
-__all__ = ["get_auth_token"]
